9deafa0d27f0ce9532ce5c4bac02d1ec9bb5b9ea
Alan Harper <alan@aussiegeek.net>
2013-08-13 17:16:13 +1000

    Switch to mysql

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rails', '4.0.0'
-gem 'pg'
+gem 'mysql2'
 
 gem 'sass-rails', '~> 4.0.0'
 gem 'uglifier', '>= 1.3.0'
--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20130812081056) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "competitions", force: true do |t|
     t.string   "title"
     t.datetime "entries_open_at"
