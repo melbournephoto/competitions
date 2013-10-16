ba1b01cc6fdb2b7c25968342af57f157f4dbd9b9
Alan Harper <alan@aussiegeek.net>
2013-10-16 21:35:52 +1100

    Add rating to entries

--- db/migrate/20130812081056_create_entries.rb
+++ db/migrate/20130812081056_create_entries.rb
@@ -4,6 +4,7 @@ class CreateEntries < ActiveRecord::Migration
       t.references :user, index: true
       t.references :competition, index: true
       t.references :section, index: true
+      t.references :rating
       t.string :title
       t.string :photo
 
