2ad8909b3d8bdf6ca5a2a22ed6e9aea5a0d17478
Alan Harper <alan@aussiegeek.net>
2013-11-25 20:55:27 +1100

    Move uploaded file path

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -6,7 +6,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
   storage :file
 
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "system/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   version :thumb do
