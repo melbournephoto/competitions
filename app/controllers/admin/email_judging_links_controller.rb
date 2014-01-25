7146372e5cb08b61449f6a561d0f804792f9ab75
Alan Harper <alan@aussiegeek.net>
2014-01-25 15:38:22 +1100

    Redirect to admin competition page after emailing judging link

--- app/controllers/admin/email_judging_links_controller.rb
+++ app/controllers/admin/email_judging_links_controller.rb
@@ -10,7 +10,7 @@ class Admin::EmailJudgingLinksController < AdminController
     @email_judging_link.email = params[:email_judging_link][:email]
     if @email_judging_link.valid?
       JudgingMailer.links(@email_judging_link.email, @competition).deliver
-      redirect_to @competition, notice: 'Judging link sent to ' + @email_judging_link.email
+      redirect_to admin_competition_path(@competition), notice: 'Judging link sent to ' + @email_judging_link.email
     else
       render action: 'new'
     end
