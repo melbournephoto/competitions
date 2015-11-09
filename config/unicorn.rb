18f50da317561b61511b89b6e599b18b8ebfc918
Alan Harper <alan@aussiegeek.net>
2015-11-09 22:10:54 +1100

    Change unicorn config back to traditional deployment

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,12 +1,13 @@
 worker_processes Integer(ENV['UNICORN_WORKERS'] || 2)
 timeout Integer(ENV['UNICORN_TIMEOUT'] || 35)
-working_directory "/app"
+
+working_directory '/srv/www/competitions/current'
 preload_app true
 
 listen "/tmp/web_server.sock", :backlog => Integer(ENV['UNICORN_BACKLOG'] || 64)
-pid '/tmp/web_server.pid'
-stderr_path "/app/log/unicorn.stderr.log"
-stdout_path "/app/log/unicorn.stdout.log"
+pid '/srv/www/competitions/shared/tmp/pids/unicorn.pid'
+stderr_path '/srv/www/competitions/current/log/unicorn.stderr.log'
+stdout_path '/srv/www/competitions/current/log/unicorn.stdout.log'
 
 GC.respond_to?(:copy_on_write_friendly=) and
   GC.copy_on_write_friendly = true
